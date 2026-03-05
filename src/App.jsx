ab55351b2059bac84c1fc296b02447bcc1c3aadd
Md. Saiful Islam Arif <si.arif.cse@gmail.com>
2026-03-06 02:20:58 +0600

    Navbar Done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Banner from './components/Banner/Banner'
+import NavBar from './components/NavBar/NavBar'
+
+function App() {
+
+  return (
+    <div className='min-h-screen bg-gray-100 text-base-content'>
+      <NavBar></NavBar>
+      <Banner></Banner>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import leftImg from "../../assets/Group-2.png";
+import rightImg from "../../assets/Group-1.png";
+
+const Banner = () => {
+    return (
+        <div className='min-h-screen w-full bg-[#f4f4f4] flex flex-col items-center py-10 gap-8'>
+            <div className='min-h-screen w-full bg-[#f4f4f4] flex flex-col items-center py-10 gap-8'>
+                <h3>In-Progress</h3>
+                <h1>0</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
